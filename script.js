7d119a57dee7ce839809a3139fad39870de62b63
dbhatti00 <90341825+dbhatti00@users.noreply.github.com>
2024-09-29 00:40:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+ import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
