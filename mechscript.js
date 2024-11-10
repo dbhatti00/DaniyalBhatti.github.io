10263596e13f6efa2473bd2444c0a5a0a6f09b0f
dbhatti00 <90341825+dbhatti00@users.noreply.github.com>
2024-11-10 06:47:36 -0700

    Add files via upload

---
+++ mechscript.js
@@ -0,0 +1,25 @@
+
+let renderImage = document.getElementById('renderImage');
+let wireframeImage = document.getElementById('wireframeImage');
+let isWireframeVisible = false; // Track if the wireframe is visible
+
+
+window.onload = function() {
+    document.getElementById("image-toggle").onclick = toggleImage;
+}
+
+const backbutton = document.getElementById('back');
+
+backbutton.addEventListener('click',function(){
+    window.location.href = 'portfolio.html';
+})
+
+function toggleImage() {
+    var image = document.getElementById("image-toggle");
+    
+    if (image.src.includes("mech.png")) {
+        image.src = "models/mechwire.png"; // Replace with the path to your wireframe image
+    } else {
+        image.src = "models/mech.png"; // Replace with the path to your render image
+    }
+}
