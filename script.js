23ccf126aeebb5af1138de1efd937b093a4a9522
dbhatti00 <90341825+dbhatti00@users.noreply.github.com>
2024-11-14 22:03:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,299 @@
-import*as THREE from"three";import{GLTFLoader}from"three/addons/loaders/GLTFLoader.js";import{DRACOLoader}from"three/addons/loaders/DRACOLoader.js";const renderer=new THREE.WebGLRenderer({antialias:!0}),scene=new THREE.Scene,camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1e3);camera.position.z=1,camera.position.y=.5,renderer.setSize(window.innerWidth,window.innerHeight),document.body.appendChild(renderer.domElement);const loadingElement=document.getElementById("loading"),pdfdownload=document.getElementById("resumedownload"),emailcopy=document.getElementById("Email"),linkedinbutton=document.getElementById("linkedin"),closeButton=document.getElementById("close-about"),modelDiv=document.getElementById("model");modelDiv.appendChild(renderer.domElement);const model=document.getElementById("model"),aboutWindow=document.getElementById("about-window"),name=document.querySelector(".name"),aboutButton=document.getElementById("about"),portfolioButton=document.getElementById("projects");scene.background=new THREE.Color(16119260);const intersectionPoint=new THREE.Vector3,target=new THREE.Object3D,planeNormal=new THREE.Vector3,plane=new THREE.Plane,raycaster=new THREE.Raycaster,mousePosition=new THREE.Vector2;let mixer,armAnimationClip,headBone,tabletModel,followMouse=!0;const newRotationX=THREE.MathUtils.degToRad(90),newRotationY=THREE.MathUtils.degToRad(0),newRotationZ=THREE.MathUtils.degToRad(180);loadingElement.style.display="block",portfolioButton.classList.add("disabled"),aboutButton.classList.add("disabled");const dracoLoader=new DRACOLoader;dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.7/");const loader=(new GLTFLoader).setPath("/Models/");loader.setDRACOLoader(dracoLoader),loader.load("Me12.glb",(e=>{const t=e.scene,o=(new THREE.Box3).setFromObject(t),n=new THREE.Vector3;o.getCenter(n),t.position.sub(n),t.scale.set(1,1,1),tabletModel=e.scene.getObjectByName("tablet"),scene.add(t),portfolioButton.classList.remove("disabled"),aboutButton.classList.remove("disabled");const i=e.animations,a="metarigAction";if(armAnimationClip=i.find((e=>e.name===a)),armAnimationClip){if(i&&i.length){mixer=new THREE.AnimationMixer(t);const e=mixer.clipAction(i.find((e=>"Key.003Action"===e.name)));e&&e.play()}headBone=t.getObjectByName("head"),headBone.rotation.x=Math.PI/2}else console.error(`Arm animation clip not found for: "${a}". Please double-check the animation name.`)}),void 0,(e=>{console.error("Model loading error:",e)}));let lastUpdateTime=0;function onWindowResize(){camera.aspect=window.innerWidth/window.innerHeight,camera.updateProjectionMatrix(),renderer.setSize(window.innerWidth,window.innerHeight)}function debounce(e,t){let o;return function(...n){clearTimeout(o),o=setTimeout((()=>e.apply(this,n)),t)}}window.addEventListener("mousemove",(function(e){const t=Date.now();t-lastUpdateTime<16||(lastUpdateTime=t,mousePosition.x=e.clientX/window.innerWidth*2-1,mousePosition.y=-e.clientY/window.innerHeight*2.5+1,planeNormal.copy(camera.position).normalize(),plane.setFromNormalAndCoplanarPoint(planeNormal,scene.position),raycaster.setFromCamera(mousePosition,camera),raycaster.ray.intersectPlane(plane,intersectionPoint),target.position.set(intersectionPoint.x,intersectionPoint.y,2))})),window.addEventListener("resize",debounce(onWindowResize,200));let isAnimating=!0;function animate(){isAnimating&&(requestAnimationFrame(animate),mixer&&mixer.update(.01),followMouse&&headBone&&headBone.lookAt(target.position),renderer.render(scene,camera))}async function loadGSAP(){const{gsap:e}=await import("gsap");return e}function startAnimation(){isAnimating=!0,animate()}function stopAnimation(){isAnimating=!1}startAnimation(),aboutButton.addEventListener("click",(function(){followMouse=!1;const e=new THREE.Vector3(Math.PI/1.65,0,0);model.classList.add("move-right"),aboutWindow.classList.add("show");const t=()=>{if(headBone.rotation.x+=.05*(e.x-headBone.rotation.x),headBone.rotation.y+=.05*(e.y-headBone.rotation.y),headBone.rotation.z+=.05*(e.z-headBone.rotation.z),Math.abs(headBone.rotation.x-e.x)>.01||Math.abs(headBone.rotation.y-e.y)>.01||Math.abs(headBone.rotation.z-e.z)>.01)requestAnimationFrame(t);else if(armAnimationClip){mixer.clipAction(armAnimationClip).play()}else console.error("Arm animation clip not found")};t(),linkedinbutton.addEventListener("click",(function(){window.open("https://www.linkedin.com/in/daniyal-bhatti/","_blank")})),pdfdownload.addEventListener("click",(function(){window.open("/Models/Daniyal_Bhatti_Resume.pdf","_blank")})),emailcopy.addEventListener("click",(function(){const e="danibatty00@gmail.com",t=document.createElement("input");t.value=e,document.body.appendChild(t),t.select(),t.setSelectionRange(0,99999),document.execCommand("copy"),document.body.removeChild(t),alert("Custom text copied to clipboard: "+e)}))})),closeButton.addEventListener("click",(function(){if(armAnimationClip){mixer.clipAction(armAnimationClip).stop()}followMouse=!0,model.classList.remove("move-right"),aboutWindow.classList.remove("show")})),document.getElementById("projects").addEventListener("click",(async function(){gsap.to(tabletModel.rotation,{x:newRotationX,y:newRotationY,z:newRotationZ,duration:1.5,ease:"power1.inOut"}),gsap.to(camera.position,{z:.1,x:-.08,y:.08,duration:2,ease:"power1.inOut"}),gsap.to(camera.rotation,{x:-.5,duration:2,ease:"power1.inOut"}),setTimeout((function(){window.location.href="portfolio.html"}),1800),aboutWindow.classList.remove("show"),model.classList.remove("move-right"),name.style.transform="translateY(-100)",name.style.opacity="0",aboutButton.style.opacity="0",portfolioButton.style.opacity="0"}));
+import * as THREE from './models/three/build/three.module.js';
+import { GLTFLoader } from './models/three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from './models/three/examples/jsm/loaders/DRACOLoader.js';
+
+//old import method:
+//import * as THREE from 'three';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+
+const scene = new THREE.Scene();
+// Set up a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 1;
+camera.position.y = .5;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const loadingElement = document.getElementById('loading');
+const pdfdownload = document.getElementById('resumedownload');
+const emailcopy = document.getElementById('Email');
+const linkedinbutton = document.getElementById('linkedin');
+const closeButton = document.getElementById('close-about');
+const modelDiv = document.getElementById('model');
+modelDiv.appendChild(renderer.domElement);
+const model = document.getElementById('model');
+const aboutWindow = document.getElementById('about-window');
+const name = document.querySelector('.name'); 
+const aboutButton = document.getElementById('about');
+const portfolioButton = document.getElementById('projects');
+
+scene.background = new THREE.Color(0xf5f5dc);
+const intersectionPoint = new THREE.Vector3();
+const target = new THREE.Object3D();
+const planeNormal = new THREE.Vector3();
+const plane = new THREE.Plane();
+const raycaster = new THREE.Raycaster();
+const mousePosition = new THREE.Vector2();
+let mixer;
+let armAnimationClip;
+
+let headBone;  
+let followMouse = true; 
+
+let tabletModel;
+let tabletModel2;
+const newRotationX = THREE.MathUtils.degToRad(90); 
+const newRotationY = THREE.MathUtils.degToRad(0);  
+const newRotationZ = THREE.MathUtils.degToRad(180); 
+const newRotationX2 = THREE.MathUtils.degToRad(90); 
+const newRotationY2 = THREE.MathUtils.degToRad(0);  
+const newRotationZ2 = THREE.MathUtils.degToRad(180);
+
+loadingElement.style.display = 'block';
+
+portfolioButton.classList.add('disabled');
+aboutButton.classList.add('disabled');
+
+const dracoLoader = new DRACOLoader();
+
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/'); // Update this path
+let loader = new GLTFLoader().setPath('/Models/');
+loader.setDRACOLoader(dracoLoader);
+
+
+
+loader.load('Me12.glb', (glb) => {
+
+  const mesh = glb.scene;
+  console.log(glb.scene);
+    // Center the model
+    const box = new THREE.Box3().setFromObject(mesh);
+    const center = new THREE.Vector3();
+    box.getCenter(center);
+    mesh.position.sub(center);
+
+    // Scale the model
+    mesh.scale.set(1, 1, 1);
+    tabletModel = glb.scene.getObjectByName("tablet");
+  
+    
+    scene.add(mesh);
+    portfolioButton.classList.remove('disabled');
+    aboutButton.classList.remove('disabled');
+const animations = glb.animations;
+const armAnimationName = 'metarigAction';
+
+armAnimationClip = animations.find(animation => animation.name === armAnimationName);
+
+if (!armAnimationClip) {
+    console.error(`Arm animation clip not found for: "${armAnimationName}". Please double-check the animation name.`);
+    return; 
+}
+if (animations && animations.length) {
+    mixer = new THREE.AnimationMixer(mesh); 
+    
+    const blinkAction = mixer.clipAction(animations.find(clip => clip.name === 'Key.003Action')); // Change this line
+   
+    if (blinkAction) {
+        blinkAction.play(); // Play the blink animation
+    }
+}
+
+    headBone = mesh.getObjectByName('head');
+   headBone.rotation.x = Math.PI / 2;
+        
+}, undefined, (error) => {
+    console.error('Model loading error:', error);
+});
+
+let lastUpdateTime = 0;
+
+window.addEventListener('mousemove', function(e) {
+    const now = Date.now();
+    if (now - lastUpdateTime < 16) return; // Update every ~16ms (60fps)
+    lastUpdateTime = now;
+
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2.5 + 1;
+    planeNormal.copy(camera.position).normalize();
+    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    raycaster.setFromCamera(mousePosition, camera);
+    raycaster.ray.intersectPlane(plane, intersectionPoint);
+    target.position.set(intersectionPoint.x, intersectionPoint.y, 2);
+   
+});
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', debounce(onWindowResize, 200));
+
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
+// Animate the head to follow the mouse
+let isAnimating = true; // Default is true if there's an initial animation
+function animate() {
+    if (isAnimating) {
+        requestAnimationFrame(animate);
+        if (mixer) mixer.update(0.01);
+        if (followMouse && headBone) headBone.lookAt(target.position);
+        renderer.render(scene, camera);
+    }
+}
+
+
+async function loadGSAP() {
+    const { gsap } = await import('gsap');
+    return gsap;
+}
+
+function startAnimation() {
+    isAnimating = true;
+    animate();
+}
+
+
+
+// Start and stop as needed
+startAnimation();
+
+     
+       
+          aboutButton.addEventListener('click', function() {
+           
+            
+            followMouse = false; // Disable mouse following
+            const targetHeadRotation = new THREE.Vector3(Math.PI / 1.65, 0, 0);
+            // Move the model to the right
+            model.classList.add('move-right');
+            
+            // Show the "About" window
+            aboutWindow.classList.add('show');
+           
+
+            const animateHeadRotation = () => {
+                headBone.rotation.x += (targetHeadRotation.x - headBone.rotation.x) * 0.05; // Smooth transition
+                headBone.rotation.y += (targetHeadRotation.y - headBone.rotation.y) * 0.05;
+                headBone.rotation.z += (targetHeadRotation.z - headBone.rotation.z) * 0.05;
+        
+                if (Math.abs(headBone.rotation.x - targetHeadRotation.x) > 0.01 ||
+                    Math.abs(headBone.rotation.y - targetHeadRotation.y) > 0.01 ||
+                    Math.abs(headBone.rotation.z - targetHeadRotation.z) > 0.01) {
+                    requestAnimationFrame(animateHeadRotation); // Keep animating until close enough
+                }
+                else {
+                   
+                    if (armAnimationClip) {
+                        const action = mixer.clipAction(armAnimationClip);
+                        action.play(); // Play the arm animation
+                    } else {
+                        console.error("Arm animation clip not found");
+                    }
+                }
+            };
+            animateHeadRotation();
+            linkedinbutton.addEventListener('click', function() {
+                window.open('https://www.linkedin.com/in/daniyal-bhatti/', '_blank');
+            })
+            
+            pdfdownload.addEventListener('click', function() {
+                window.open('/Models/Daniyal_Bhatti_Resume.pdf', '_blank');
+            })
+            
+            emailcopy.addEventListener('click', function() {
+                const customText = "danibatty00@gmail.com";
+            
+                  
+                  const tempInput = document.createElement('input');
+                  tempInput.value = customText;
+                  
+               
+                  document.body.appendChild(tempInput);
+                  tempInput.select();
+                  tempInput.setSelectionRange(0, 99999);  
+            
+                  
+                  document.execCommand('copy');
+            
+                
+                  document.body.removeChild(tempInput);
+            
+                
+                  alert('Custom text copied to clipboard: ' + customText);
+            })
+
+        });
+         
+        closeButton.addEventListener('click', function() {
+            if (armAnimationClip) {
+                const action = mixer.clipAction(armAnimationClip);
+                action.stop(); // Play the arm animation
+            }
+            followMouse = true;
+            model.classList.remove('move-right');
+            aboutWindow.classList.remove('show'); 
+        });
+
+        
+
+        document.getElementById('projects').addEventListener('click', async function() {
+           
+   
+   
+  
+
+
+           
+gsap.to(tabletModel.rotation, {
+    x: newRotationX,
+    y: newRotationY,
+    z: newRotationZ,
+    duration: 1.5,  
+    ease: "power1.inOut"
+});
+
+
+
+gsap.to(camera.position, {
+    
+   
+    z: 0.1,
+    x: -0.08,
+    y: 0.08,
+    duration: 2, 
+    ease: "power1.inOut",
+    
+});
+gsap.to(camera.rotation, {
+    
+    
+    x:-.5,
+    duration: 2, 
+    ease: "power1.inOut", 
+    
+});
+
+setTimeout(function() {
+    window.location.href = 'portfolio.html'; 
+  }, 1800); 
+  aboutWindow.classList.remove('show');
+  model.classList.remove('move-right');
+  name.style.transform = 'translateY(-100)'; // Move the name up
+  name.style.opacity = '0'; // Fade out
+  aboutButton.style.opacity = '0'; // Fade out
+  portfolioButton.style.opacity = '0';
+
+})
