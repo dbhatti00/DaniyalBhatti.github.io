4e95c37d3053ed24e56cc8675559bc271a0aa5e8
dbhatti00 <90341825+dbhatti00@users.noreply.github.com>
2024-12-10 01:09:55 -0700

    Add files via upload

--- script2.js
+++ script2.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
     images.forEach(image => observer.observe(image));
 });
 
-document.getElementById('chair').addEventListener('click', function() {
-    window.location.href = 'chair.html'; 
+document.getElementById('orc').addEventListener('click', function() {
+    window.location.href = 'orc.html'; 
 })
 document.getElementById('kid').addEventListener('click', function() {
     window.location.href = 'kid.html'; 
