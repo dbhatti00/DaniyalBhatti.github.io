9edd5dbe568aa985db23afd9633f7242a259b7fe
dbhatti00 <90341825+dbhatti00@users.noreply.github.com>
2025-07-09 05:22:48 -0600

    Update Tiefling.js

--- Tiefling.js
+++ Tiefling.js
@@ -1,88 +1,10 @@
-import * as THREE from './models/three/three/build/three.module.js';
-import { GLTFLoader } from './models/three/three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from './models/three/three/examples/jsm/loaders/DRACOLoader.js';
-import { OrbitControls } from './models/three/three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
 const zoomableImages = document.querySelectorAll('.zoomable');
 const overlay = document.getElementById('imageZoomOverlay');
 const zoomedImg = document.getElementById('zoomedImage');
 let scale = 1;
 
 
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidght / window.innerHeight, 0.1, 1000);
-
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-
-let object;
-
-let controls;
-
-let objToRender = 'Tiefling';
-
-
-scene.background = new THREE.Color(0x222220);
-const dracoLoader = new DRACOLoader();
-
-
-dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/'); // Update this path
-let loader = new GLTFLoader().setPath('/');
-loader.setDRACOLoader(dracoLoader);
-loader.load('tiefling.glb', (glb) => {
-    //If the file is loaded, add it to the scene
-    object = glb.scene;
-    scene.add(object);
-const loaderDiv = document.getElementById('loader');
-    if (loaderDiv) loaderDiv.style.display = 'none';
-  },
-  function (xhr) {
-    // Optional: track loading progress
-    console.log(`${(xhr.loaded / xhr.total * 100).toFixed(0)}% loaded`);
-  },
-  function (error) {
-    console.error('An error happened', error);
-  }
-);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-//Add the renderer to the DOM
-document.getElementById("container3D").appendChild(renderer.domElement);
-
-
-
-
-if (objToRender === "Tiefling") {
-  controls = new OrbitControls(camera, renderer.domElement);
- // Set initial camera position
-  camera.rotation.x = -0.2;
-  camera.position.set(0, 1.3, 1.3);
-
-  // Set the point you want the camera to orbit around
-  controls.target.set(0, 1, 0); // This is usually the model's head or center
-
-  // Apply the target immediately
-  controls.update();
-}
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener('resize', onWindowResize, false);
-onWindowResize(); // Call it once at the start too
-function animate() {
-  requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
-
-  renderer.render(scene, camera);
-}
-animate();
 
 zoomableImages.forEach(img => {
   img.addEventListener('click', () => {
